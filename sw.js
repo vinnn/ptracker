7cdde7f4d1c1c9d6aba2a50a1798af7dfc8b4bd6
vinnn <vmoutel@yahoo.fr>
2024-01-05 21:34:21 +0000

    wip

--- sw.js
+++ sw.js
@@ -7,7 +7,7 @@ const CACHE_NAME = `period-tracker-${VERSION}`;
 // The static resources that the app needs to function.
 const APP_STATIC_RESOURCES = [
   "./",
-  "./ptracker/",
+//   "./ptracker/",
   "./index.html",
   "./app.js",
   "./style.css",
